6d2bfe2b540a50d9fa4cd27c1831eed96bacd73c
tobilobaadeyemo <tobyadeyemo@gmail.com>
2025-11-25 15:23:55 +0200

    Delete tidy_agent.py

--- tidy_agent.py
+++
@@ -1,5 +0,0 @@
-# tidy_agent.py
-def adjust_schedule(current_schedule, drift):
-    # Moves tasks based on drift calculation
-    adjusted_schedule = current_schedule  # modify as needed
-    return adjusted_schedule
