d4caddf1ed78192afd1cee604c2b0b5b74d0ca70
tobilobaadeyemo <tobyadeyemo@gmail.com>
2025-11-25 15:23:17 +0200

    Delete meet_agent.py

--- meet_agent.py
+++
@@ -1,5 +0,0 @@
-# meet_agent.py
-def handle_meeting_conflict(existing_meeting, new_meeting):
-    if new_meeting["time"] == existing_meeting["time"]:
-        return "Conflict detected. Moving meeting"
-    return "Meeting scheduled"
