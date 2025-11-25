e84c961d57d82d1ec401ca5959880bd6caa2d85f
tobilobaadeyemo <tobyadeyemo@gmail.com>
2025-11-25 15:22:59 +0200

    Delete lead_agent.py

--- lead_agent.py
+++
@@ -1,7 +0,0 @@
-# lead_agent.py
-def lead_agent(user_input):
-    # Processes user input and routes to the correct agent
-    if "move" in user_input:
-        return "Moving the task"
-    elif "schedule" in user_input:
-        return "Scheduling a meeting"
